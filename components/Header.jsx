3612106441e79a35c0a637382414063936793446
ShIvAmKaUsHaL <9720sk41@gmail.com>
2025-02-25 17:57:20 +0530

    header added + blogs dynamic pages added

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="w-full min-h-20 bg-[#FFFFFF66] sticky">
+    <div className="max-w-[1280px] justify-between items-center flex flex-row mx-auto py-4">
+      <img src='/logo.png' className="" alt="logo" />
+      <div className="flex flex-row gap-12 text-xl color-[#05363B] font-medium">
+        <Link href='#'>Home</Link>
+        <Link href='#'>About Us</Link>
+        <Link href='#'>Bunkering</Link>
+        <Link href='#'>Other Services</Link>
+        <Link href='#'>Blogs</Link>
+        <Link href='#'>Contact Us</Link>
+      </div>
+    </div>
+  </div>
+  )
+}
