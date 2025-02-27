8262c9541c195618601d8d6ca9691ceed70d22ac
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-27 22:16:40 +0530

    added blogs metadata

--- src/app/(root)/blogs/[id]/page.js
+++ src/app/(root)/blogs/[id]/page.js
@@ -1,9 +1,13 @@
 import React, { Suspense } from 'react'
 import pool from '../../../../../lib/Db';
+import { notFound } from 'next/navigation';
 
 export default async function page({params}) {
     const id = (await params).id
     const rows = await pool.query('SELECT * FROM blogs WHERE id = ?', id);
+    if (rows[0].length === 0) {
+      notFound(); // Redirects to 404 page
+  }
     const blog = rows[0][0];
   return (
       <Suspense fallback={<p className="text-black">Loading...</p>}>
@@ -15,4 +19,23 @@ export default async function page({params}) {
           <p>Admin</p>
         </Suspense>
   )
+}
+
+export async function generateMetadata({ params }) {
+  const id = (await params).id;
+  const rows = await pool.query('SELECT * FROM blogs WHERE id = ?', [id]);
+
+  if (rows[0].length === 0) {
+      return {
+          title: 'Blog Not Found | Acqalis Marine',
+          description: 'This blog does not exist.',
+      };
+  }
+
+  const blog = rows[0][0];
+
+  return {
+      title: `${blog.title} | Acqalis Marine`,
+      description: `${blog.shortdesc} | Acqalis Marine`,
+  };
 }
