4da7d3efb0ea664ea7c581e1302fca76aae9ffb1
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-27 13:15:52 +0530

    added hamburger menu for mobile

--- components/Hamburger.jsx
+++ components/Hamburger.jsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link';
 import React, { useState } from 'react'
 
 export default function Hamburger() {
@@ -15,10 +16,12 @@ export default function Hamburger() {
       {isOpen && (
         <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg overflow-hidden">
           <ul className="flex flex-col">
-            <li className="p-3 hover:bg-gray-200 cursor-pointer">Home</li>
-            <li className="p-3 hover:bg-gray-200 cursor-pointer">About</li>
-            <li className="p-3 hover:bg-gray-200 cursor-pointer">Services</li>
-            <li className="p-3 hover:bg-gray-200 cursor-pointer">Contact</li>
+          <Link href='/'><li className="p-3 hover:bg-gray-200 cursor-pointer">Home</li></Link>
+          <Link href='/#about'><li className="p-3 hover:bg-gray-200 cursor-pointer">About Us</li></Link>
+          <Link href='/#bunkering'><li className="p-3 hover:bg-gray-200 cursor-pointer">Bunkering</li></Link>
+          <Link href='/#services'><li className="p-3 hover:bg-gray-200 cursor-pointer">Other Services</li></Link>
+          <Link href='/blogs'><li className="p-3 hover:bg-gray-200 cursor-pointer">Blogs</li></Link>
+          <Link href='/#contact'><li className="p-3 hover:bg-gray-200 cursor-pointer">Contact Us</li></Link>
           </ul>
         </div>
       )}
