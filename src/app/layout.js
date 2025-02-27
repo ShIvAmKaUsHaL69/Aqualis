7cd96dbf7408a2ddba4f4a55c3b875878dbc3398
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-27 14:19:19 +0530

    font for all users

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,12 @@ const poppins = Poppins({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
