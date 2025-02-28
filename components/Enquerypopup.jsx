243e8fed420fe7aa0b7c2a5264c87a1d181ae720
ShIvAmKaUsHaL <9720sk41@gmail.com>
2025-02-28 13:50:57 +0530

    fixed union image

--- components/Enquerypopup.jsx
+++ components/Enquerypopup.jsx
@@ -23,7 +23,7 @@ export default function Enquerypopup() {
         <button onClick={handlepopup} className='bg-[#05363B] text-white text-xl px-10 py-3 rounded-md'>Send Enquiry</button>
         <div className='absolute w-full h-full bg-[#00000024] top-0 left-0'>
         <div className='max-w-[1280px] mx-auto flex items-end justify-end pb-32 lg:pb-48 px-4 lg:px-40 w-full h-full'>
-      <div className='bg-[url(/union.png)] h-[15rem] w-[350px] bg-contain bg-no-repeat py-10 flex items-center justify-center flex-col px-10'>
+      <div className='bg-[url(/Union.png)] h-[15rem] w-[350px] bg-contain bg-no-repeat py-10 flex items-center justify-center flex-col px-10'>
       <p className='text-xs'>if you are sending an enquiry pls ensure to indicate the vessel name, buying entity, grade & specs of product, date range and quantity range in metric tones.</p>
       <button onClick={handlepopup} className='bg-[#05363B] text-white text-base px-5 py-2 lg:mt-4 mt-2 rounded-md'>Okay</button>
       </div>
