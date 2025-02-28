4f967bb2e24cbd767bd92dc7694b43ea796e4df0
ShIvAmKaUsHaL <9720sk41@gmail.com>
2025-02-28 14:01:10 +0530

    footer fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -18,12 +18,12 @@ export default function Footer() {
         <div className='max-w-[1280px] mx-auto w-full h-full px-3 py-20 flex flex-col gap-10 lg:gap-0 lg:flex-row xl:justify-between sm:justify-around'>
           <div>
             <img src='/whitelogo.png' alt='white-logo' />
-            <p className='text-white w-full lg:max-w-[300px] mt-7'>Aqualis Marine leverages an agile and strategic approach to deliver transparent, customized maritime solutions, enhancing efficiency and value for our clients</p>
+            <p className='text-white w-full lg:max-w-[300px] mt-7 text-xs'>Aqualis Marine leverages an agile and strategic approach to deliver transparent, customized maritime solutions, enhancing efficiency and value for our clients</p>
           </div>
           <div className='flex flex-col lg:flex-row gap-10 lg:gap-32'>
             <div className=''>
-              <p className='font-bold text-white'>PAGES</p>
-              <div className='lg:mt-10 mt-2 text-white flex flex-col gap-2'>
+              <p className='font-bold text-white text-sm'>PAGES</p>
+              <div className='lg:mt-10 mt-2 text-xs text-white flex flex-col gap-2'>
                 <Link href='/#about'>About us</Link>
                 <Link href='/#bunkering'>Bunkering</Link>
                 <Link href='/#services'>Other Services</Link>
@@ -32,8 +32,8 @@ export default function Footer() {
               </div>
             </div>
             <div className=''>
-              <p className='font-bold text-white'>IMPORANT LINKS</p>
-              <div className='lg:mt-10 mt-2 text-white flex flex-col gap-2'>
+              <p className='font-bold text-white text-sm'>IMPORANT LINKS</p>
+              <div className='lg:mt-10 mt-2 text-white text-xs flex flex-col gap-2'>
                 <Link href='#about'>Terms & Conditions</Link>
                 <Link href='#about'>Privacy Policy</Link>
                 <Link href='#about'>Legal Disclaimer</Link>
@@ -41,8 +41,8 @@ export default function Footer() {
               </div>
             </div>
             <div className=''>
-              <p className='font-bold text-white'>CONTACT INFORMATION</p>
-              <div className='lg:mt-10 mt-2 text-white flex flex-col gap-2'>
+              <p className='font-bold text-white text-sm'>CONTACT INFORMATION</p>
+              <div className='lg:mt-10 mt-2 text-white flex flex-col text-xs gap-2'>
                 <Link href='mailto:contact@aqualismarine.com'>Email :       contact@aqualismarine.com</Link>
                 <Link href='tel:+6597399507'>Phone :     (65) 97399507</Link>
                 <Link href='#about'>linkdin</Link>
@@ -52,8 +52,8 @@ export default function Footer() {
         </div>
       </div>
       <div className='bg-[#05363B] relative'>
-        <div className='max-w-[1280px] mx-auto w-full h-full px-3 py-10 text-white'>
-          <p>CopyRights : Aqualis Marine@2025</p>
+        <div className='max-w-[1280px] mx-auto w-full h-full px-3 py-10 text-xs text-white'>
+          <p >CopyRights : Aqualis Marine@2025</p>
           <Link href='https://api.whatsapp.com/send/?phone=6597399507&text=Hello+%2C+i+am+interested&type=phone_number&app_absent=0' className='absolute top-[20%] right-[5%]'>
           <img src='/whatsapp.png' alt='Whatsapp-icon' />
           </Link>
