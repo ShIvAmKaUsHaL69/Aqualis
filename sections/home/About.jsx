cdc530ff175ab659d32e685d70317ae1afea97ec
ShIvAmKaUsHaL <9720sk41@gmail.com>
2025-03-03 12:21:50 +0530

    changes in about section

--- sections/home/About.jsx
+++ sections/home/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='bg-[url(/Aboutbackground.png)] bg-no-repeat bg-cover pt-10 lg:pt-40' id='about'>
+    <div className='bg-[url(/Aboutbackground.png)] min-h-[100vh] flex items-center bg-no-repeat bg-cover pt-10 lg:pt-28' id='about'>
       <div className='max-w-[1280px] mx-auto w-full flex flex-col lg:pb-48 items-start gap-3 px-3'>
       <div className="block lg:flex flex-row w-full gap-80">
         <div className="relative w-full lg:w-1/2">
@@ -13,7 +13,7 @@ export default function About() {
             className="lg:block hidden absolute -bottom-24 -right-64 border-4 border-white shadow-lg"
             />
         </div>
-        <div className="flex w-full lg:w-1/2 lg:mt-0 mt-10 flex-col gap-10">
+        <div className="flex w-full lg:w-1/2 lg:mt-0 mt-10 flex-col gap-5">
             <p className="text-4xl font-bold text-[#05363B]">About Us</p>
             <p className='text-[#00000099]'>At Aqualis Marine, our name reflects our ethos. "Aqualis," meaning "water-like," symbolizes adaptability and resilience.</p>
             <p className='text-[#00000099]'>Specializing in transparent bunker broking, we provide expert fuel procurement across Singapore, Asia-Pacific and Indian Subcontinent. <span className='font-semibold'>We bridge the gap for shipowners and traders in different time zones</span>, ensuring access to reliable, cost-effective, and compliant bunkers with real-time market insights.</p>
