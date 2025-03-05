4f08622444fcc6426817e691201fd1f0a637bb07
ShIvAmKaUsHaL <9720sk41@gmail.com>
2025-03-05 10:01:31 +0530

    padding hero section

--- sections/home/Entry.jsx
+++ sections/home/Entry.jsx
@@ -4,7 +4,7 @@ import Videobg from '../../components/Videobg'
 
 export default function Entry() {
   return (
-    <div className='min-h-[100vh] overflow-hidden relative w-full flex items-end pb-2'>       
+    <div className='min-h-[100vh] overflow-hidden relative w-full flex items-end pb-4'>       
         <Videobg />
       <div className='lg:px-[13.5%] mx-auto w-full flex flex-col items-start gap-3 px-5 relative z-10'>
         <p className='py-2 bg-[#00000066] text-2xl xxl:text-4xl text-white rounded-md px-4'>Offering Efficient and Reliable Fueling Services</p>
